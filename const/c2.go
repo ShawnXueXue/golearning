c236e3af2fc6dcb33e7860147e55ffa0108a1a34
wgxue <wgxue@ctrip.com>
2018-09-30 16:38:26 +0800

    const

--- const/c2.go
+++ const/c2.go
@@ -14,4 +14,32 @@ func t1() {
 	fmt.Println(leafPageElementSize)
 	fmt.Println(reflect.TypeOf(a))
 	fmt.Println(b)
+
+	const (
+		a = iota    //0
+		b           //1
+		c           //2
+		d = "hello" // 3
+		e           // 4
+		f = 100     // 5
+		g           // 6
+		h = iota    // 7
+		i           // 8
+	)
+	fmt.Println(a, b, c, d, e, f, g, h, i)
+
+	const (
+		//左移0位
+		j = 1 << iota
+		//左移1位
+		k = 8 << iota
+		//左移2位
+		l
+		//左移3位
+		m
+	)
+	fmt.Println("i=", j)
+	fmt.Println("j=", k)
+	fmt.Println("k=", l)
+	fmt.Println("l=", m)
 }
