80059cbeb4ab0ec03b64c241ca7dcac05ef7ad07
wgxue <wgxue@ctrip.com>
2018-10-09 17:49:11 +0800

    test

--- puzzlers/article19/demo47.go
+++ puzzlers/article19/demo47.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
-	s1 := []int{0, 1, 2, 3, 4}
-	e5 := s1[5]
-	_ = e5
+	//s1 := []int{0, 1, 2, 3, 4}
+	//e5 := s1[5]
+	//_ = e5
+
+	for _, value := range []int{0, 1, 2, 3, 4} {
+		value *= 2
+		fmt.Println(value)
+	}
 }
