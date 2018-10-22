a0d53c76cb1e2cb5b2894f53a8cb7cae5432f9db
wgxue <wgxue@ctrip.com>
2018-10-22 21:27:29 +0800

    atomic

---
+++ puzzlers/atomic/demo63.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	num := uint32(18)
+	fmt.Printf("The number: %d\n", num)
+	delta := int32(-3)
+	atomic.AddUint32(&num, uint32(delta))
+	fmt.Printf("The number: %d\n", num)
+	// ^uint32(-N-1)
+	atomic.AddUint32(&num, ^uint32(-(-3)-1))
+	fmt.Printf("The number: %d\n", num)
+	fmt.Printf("The two's complement of %d: %b\n", delta, uint32(delta))
+	fmt.Printf("The equivalent:             %b\n", ^uint32(-(-3)-1))
+	fmt.Println()
+
+	forAndCAS1()
+	fmt.Println()
+	forAndCAS2()
+}
+
+func forAndCAS1() {
+	sign := make(chan struct{}, 2)
+	num := int32(0)
+	fmt.Printf("The number: %d\n", num)
+	go func() {
+		defer func() {
+			sign <- struct{}{}
+		}()
+		for {
+			time.Sleep(time.Millisecond * 500)
+			newNum := atomic.AddInt32(&num, 2)
+			fmt.Printf("The number: %d\n", newNum)
+			if newNum == 10 {
+				break
+			}
+		}
+	}()
+	go func() {
+		defer func() {
+			sign <- struct{}{}
+		}()
+		for {
+			if atomic.CompareAndSwapInt32(&num, 10, 0) {
+				fmt.Println("The number has gone to zero.")
+				break
+			}
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+	<-sign
+	<-sign
+}
+
+func forAndCAS2() {
+	sign := make(chan struct{}, 2)
+	num := int32(0)
+	fmt.Printf("The number: %d\n", num)
+	max := int32(20)
+	go func(id int, max int32) {
+		defer func() {
+			sign <- struct{}{}
+		}()
+		for i := 0; ; i++ {
+			currNum := atomic.LoadInt32(&num)
+			if currNum >= max {
+				break
+			}
+			newNum := currNum + 2
+			time.Sleep(time.Millisecond * 200)
+			if atomic.CompareAndSwapInt32(&num, currNum, newNum) {
+				fmt.Printf("The number: %d [%d-%d]\n", newNum, id, i)
+			} else {
+				fmt.Printf("The CAS operation failed. [%d-%d]\n", id, i)
+			}
+		}
+	}(1, max)
+
+	go func(id int, max int32) {
+		defer func() {
+			sign <- struct{}{}
+		}()
+		for j := 0; ; j++ {
+			currNum := atomic.LoadInt32(&num)
+			if currNum >= max {
+				break
+			}
+			newNum := currNum + 2
+			time.Sleep(time.Millisecond * 200)
+			if atomic.CompareAndSwapInt32(&num, currNum, newNum) {
+				fmt.Printf("The number: %d [%d-%d]\n", newNum, id, j)
+			} else {
+				fmt.Printf("The CAS operation failed. [%d-%d]\n", id, j)
+			}
+		}
+	}(2, max)
+	<-sign
+	<-sign
+}
---
+++ puzzlers/atomic/demo64.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"sync/atomic"
+)
+
+func main() {
+	// 1
+	var box atomic.Value
+	fmt.Println("copy box to box2.")
+	box2 := box
+	v1 := []int{1, 2, 3}
+	fmt.Printf("Store %v to box.\n", v1)
+	box.Store(v1)
+	fmt.Printf("The value load from box is %v.\n", box.Load())
+	fmt.Printf("The value load from box2 is %v.\n", box2.Load())
+	fmt.Println()
+
+	// 2
+	v2 := "123"
+	fmt.Printf("Store %q to box2.\n", v2)
+	box2.Store(v2)
+	fmt.Printf("The value load from box is %v.\n", box.Load())
+	fmt.Printf("The value load from box2 is %q.\n", box2.Load())
+	fmt.Println()
+
+	// 3
+	fmt.Println("copy box to box3.")
+	box3 := box
+	fmt.Printf("The value load from box3 is %v.\n", box3.Load())
+	v3 := 123
+	fmt.Printf("Store %d to box3.\n", v3)
+	//box3.Store(v3) // panic: store of inconsistently typed value into Value
+	_ = box3
+	fmt.Println()
+
+	// 4
+	var box4 atomic.Value
+	v4 := errors.New("something wrong")
+	fmt.Printf("Store an error message %q to box4.\n", v4)
+	box4.Store(v4)
+	v41 := io.EOF
+	fmt.Println("Store a value of the same type to box4.")
+	box4.Store(v41)
+	v42, ok := interface{}(&os.PathError{}).(error)
+	if ok {
+		fmt.Printf("Store a value of type %T that implements error interface to box4.\n", v42)
+		//box4.Store(v42) // panic: store of inconsistently typed value into Value
+	}
+	fmt.Println()
+
+	// 5
+	box5, err := NewAtomicValue(v4)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+	}
+	fmt.Printf("The legal type in box5 is %s.\n", box5.TypeOfValue())
+	fmt.Println("Store a value of the same type to box5.")
+	err = box5.Store(v41)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+	}
+	fmt.Printf("Store a value of type %T that implements error interface to box5.\n", v42)
+	box5.Store(v42)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+	}
+	fmt.Println()
+}
+
+type atomicValue struct {
+	v atomic.Value
+	t reflect.Type
+}
+
+func NewAtomicValue(example interface{}) (*atomicValue, error) {
+	if example == nil {
+		return nil, errors.New("atomic value: nil example")
+	}
+	return &atomicValue{
+		t: reflect.TypeOf(example),
+	}, nil
+}
+
+func (av *atomicValue) Store(v interface{}) error {
+	if v == nil {
+		return errors.New("atomic value: nil value")
+	}
+	t := reflect.TypeOf(v)
+	if t != av.t {
+		return fmt.Errorf("atomic value: wrong type: %s", t)
+	}
+	av.v.Store(v)
+	return nil
+}
+
+func (av *atomicValue) Load() interface{} {
+	return av.v.Load()
+}
+
+func (av *atomicValue) TypeOfValue() reflect.Type {
+	return av.t
+}
