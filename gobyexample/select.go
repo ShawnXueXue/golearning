9f6155ebfee00b9ea660dad833f40f151f0da969
wgxue <wgxue@ctrip.com>
2019-01-10 17:29:15 +0800

    87

--- gobyexample/select.go
+++ gobyexample/select.go
@@ -11,17 +11,33 @@ func main() {
 	go func() {
 		time.Sleep(time.Second * 1)
 		c1 <- "one"
+		time.Sleep(time.Second * 3)
+		c1 <- "three"
 	}()
 	go func() {
 		time.Sleep(time.Second * 2)
 		c2 <- "two"
+		time.Sleep(time.Second * 4)
+		c2 <- "four"
+		time.Sleep(time.Second * 1)
+		close(c2)
 	}()
-	for i := 0; i < 2; i++ {
+	for {
 		select {
 		case msg1 := <-c1:
-			fmt.Println("received", msg1)
-		case msg2 := <-c2:
-			fmt.Println("received", msg2)
+			fmt.Println("c1 received", msg1)
+		case msg2, open := <-c2:
+			if open {
+				fmt.Println("c2 received", msg2)
+			} else {
+				fmt.Println("c2 closed, exit.")
+				goto Exit
+			}
+		default:
+			time.Sleep(time.Second * 1)
+			fmt.Println("waiting...")
 		}
 	}
+Exit:
+	fmt.Println("exit select.")
 }
---
+++ puzzlers/chan2/select.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "one"
+		time.Sleep(time.Second * 3)
+		c1 <- "three"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "two"
+		time.Sleep(time.Second * 4)
+		c2 <- "four"
+		time.Sleep(time.Second * 1)
+		close(c2)
+	}()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println("c1 received", msg1)
+			case msg2, open := <-c2:
+				if open {
+					fmt.Println("c2 received", msg2)
+				} else {
+					fmt.Println("c2 closed, exit.")
+					return
+				}
+			default:
+				time.Sleep(time.Second * 1)
+				fmt.Println("waiting...")
+			}
+		}
+	}()
+	wg.Wait()
+}
