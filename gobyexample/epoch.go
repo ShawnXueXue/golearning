ad6dbd51b2c17f1881c95e11778ffb6862f05ebb
wgxue <wgxue@ctrip.com>
2019-04-02 18:38:18 +0800

    utf8

--- gobyexample/epoch.go
+++ gobyexample/epoch.go
@@ -6,6 +6,13 @@ import (
 	"time"
 )
 
+/*
+0000 0000-0000 007F | 0xxxxxxx
+0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+*/
+
 func main() {
 	now := time.Now()
 	secs := now.Unix()
