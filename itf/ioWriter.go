8cd4b604b81fc7b7e6e2cf74d4354093668187b0
ShawnXueXue <shawnglhf@gmail.com>
2019-06-22 13:58:47 +0800

    range

---
+++ itf/ioWriter.go
@@ -0,0 +1,21 @@
+package itf
+
+import "fmt"
+
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (n int, err error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func t1() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c)
+
+	c = 0
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c)
+}
